33e5557132d7e3e751789ab42b6f9cf6f57729d7
Aleksei Samoilov <samoylov.alexey.v@gmail.com>
2023-03-14 23:27:20 +0300

    Adding code for additional tasks. Made status changeable

--- Profile/ProfileHeaderView.swift
+++ Profile/ProfileHeaderView.swift
@@ -31,9 +31,9 @@ class ProfileHeaderView: UIView {
     }()
     
 //MARK: - Добавим кнопку
-    private lazy var showStatusButton: UIButton = {
+    private lazy var setStatusButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("Show status", for: .normal)
+        button.setTitle("Set status", for: .normal)
         button.backgroundColor = .systemBlue
         button.tintColor = .white
         button.layer.cornerRadius = 4
@@ -57,7 +57,21 @@ class ProfileHeaderView: UIView {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
+//MARK: - Добавим TextField для изменения статуса
+    private lazy var statusChangeTextField: UITextField = {
+       let textField = UITextField()
+        textField.placeholder = "Set your status"
+        textField.layer.cornerRadius = 12
+        textField.backgroundColor = .white
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.font = UIFont(name: "Helvetica-regular", size: 15)
+        textField.textColor = .black
+        textField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
 //MARK: - Разместим все на UIView
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -70,8 +84,9 @@ class ProfileHeaderView: UIView {
     private func setupView() {
         addSubview(profilePhoto)
         addSubview(profileName)
-        addSubview(showStatusButton)
+        addSubview(setStatusButton)
         addSubview(profileStatus)
+        addSubview(statusChangeTextField)
         setupConstraints()
     }
     
@@ -87,18 +102,31 @@ class ProfileHeaderView: UIView {
             profileName.leadingAnchor.constraint(equalTo: profilePhoto.trailingAnchor, constant: 16),
             profileName.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
             
-            showStatusButton.topAnchor.constraint(equalTo: profilePhoto.bottomAnchor, constant: 16),
-            showStatusButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            showStatusButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            showStatusButton.heightAnchor.constraint(equalToConstant: 50),
+            setStatusButton.topAnchor.constraint(equalTo: statusChangeTextField.bottomAnchor, constant: 16),
+            setStatusButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            setStatusButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            setStatusButton.heightAnchor.constraint(equalToConstant: 50),
             
-            profileStatus.bottomAnchor.constraint(equalTo: showStatusButton.topAnchor, constant: -34),
+            profileStatus.topAnchor.constraint(equalTo: profileName.bottomAnchor, constant: 52),
             profileStatus.leadingAnchor.constraint(equalTo: profilePhoto.trailingAnchor, constant: 16),
-            profileStatus.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+            profileStatus.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+            statusChangeTextField.topAnchor.constraint(equalTo: profileStatus.bottomAnchor, constant: 2),
+            statusChangeTextField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            statusChangeTextField.heightAnchor.constraint(equalToConstant: 40),
+            statusChangeTextField.leadingAnchor.constraint(equalTo: profilePhoto.trailingAnchor, constant: 16)
         ])
     }
     
     @objc func buttonStatusPressed (_ sender: UIButton) {
+        profileStatus.text = statusTextChanged(statusChangeTextField)
         print(profileStatus.text ?? "Ничего тут нет")
+        statusChangeTextField.text = nil
+    }
+    
+    @objc func statusTextChanged (_ sender: UITextField) -> String {
+        var statusText: String
+        statusText = statusChangeTextField.text ?? "nil"
+        return statusText
     }
 }
